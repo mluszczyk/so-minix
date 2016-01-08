6e1e2fa84750e860831d6652f397449da7d02528
Michal Luszczyk <mluszczyk@users.noreply.github.com>
2016-01-08 14:51:44 +0000

    Semgroup syscalls made work.

--- servers/pm/semgroup.c
+++ servers/pm/semgroup.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include "pm.h"
+#include "mproc.h"
 
 int do_getsemgroup(void) {
-    printf("Semget.\n");
-    return 0;
+    int group = mp->mp_sem_group;
+    printf("DEBUG: getsemgroup of %d -> %d.\n", who_p, group);
+    return group;
 }
 
 int do_setsemgroup(void) {
-    printf("Get sem group.\n");
+    int new_group = m_in.m1_i1;
+    printf("DEBUG: setsemgroup of %d to %d.\n", who_p, new_group);
+    mp->mp_sem_group = new_group;
     return 0;
 }
