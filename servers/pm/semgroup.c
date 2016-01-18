d561023bed59735f932303f469b3262c3442e86b
Michal Luszczyk <mluszczyk@users.noreply.github.com>
2016-01-18 17:26:36 +0000

    Cleaner way to find IPC.

--- servers/pm/semgroup.c
+++ servers/pm/semgroup.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
-#include <string.h>
 #include "pm.h"
+
+#include <minix/callnr.h>
+#include <minix/com.h>
+#include <minix/config.h>
+#include <minix/ipc.h>
+#include <minix/endpoint.h>
+#include <minix/sysutil.h>
+#include <minix/syslib.h>
+#include <minix/const.h>
+#include <minix/type.h>
+#include <minix/ds.h>
+#include <minix/rs.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <lib.h>
+
 #include "mproc.h"
 
 int do_getsemgroup(void) {
@@ -20,19 +37,28 @@ int do_setsemgroup(void) {
     return 0;
 }
 
-// Hard to get minix_rs_lookup working here. -1 means not found
-endpoint_t find_ipc() {
-	for (struct mproc *rmp = &mproc[0]; rmp < &mproc[NR_PROCS]; ++rmp) {
-		if (strcmp(rmp->mp_name, "ipc") == 0) {
-			return rmp->mp_endpoint;
-		}
+// Copied from user space library
+int minix_rs_lookup(const char *name, endpoint_t *value)
+{
+	message m;
+	size_t len_key;
+
+	len_key = strlen(name)+1;
+
+	m.RS_NAME = (char *) __UNCONST(name);
+	m.RS_NAME_LEN = len_key;
+
+	if (_syscall(RS_PROC_NR, RS_LOOKUP, &m) != -1) {
+		*value = m.RS_ENDPOINT;
+		return OK;
 	}
+
 	return -1;
 }
 
 static void notify_ipc_proc(endpoint_t pt, int group, int type) {
-	endpoint_t ipc = find_ipc();
-	if (ipc != -1) {
+	endpoint_t ipc;
+	if (minix_rs_lookup("ipc", &ipc) == OK) {
 		message m;
 		m.m_type = type;
 		m.m1_i1 = pt;
